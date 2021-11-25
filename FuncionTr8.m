7630834eae1bc3fb8d61abbb6a9fff602ab5f278
PatriciaZP <91707472+PatriciaZP@users.noreply.github.com>
2021-11-24 21:32:14 -0600

    Add files via upload

---
+++ FuncionTr8.m
@@ -0,0 +1,22 @@
+% Ocatve Script
+% Title                :Funciones trascendentes: trigonométricas, logarítmicas y exponenciales
+% Description          :Script para identificar funciones trascendentes
+% Author               :Patricia Zaragoza Palma
+% Date                 :20211124
+% Version              :1
+% Usege                :octave>cd /path/
+%                      :octave>Funcionetrascendentes
+%                      :Requiere aplicacion octave, usar su linea de comandos 
+% Notes                :Poder identificar el tipo de funcion trascendente mediante graficas 
+%                      :usar comandos 
+
+title('biyectiva')
+
+
+clear
+% Dominio de la Funcion 
+x= -50:1:50
+% Reglas de correspondencia (Funcion)
+y= log(3).*x
+% Plotear funcion 
+plot(x, y);
